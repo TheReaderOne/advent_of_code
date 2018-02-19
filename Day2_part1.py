d02169c83101f728eefa789fce56332acf1d2857
TheReaderOne <twardzikto@gmail.com>
2018-02-19 06:32:46 +0100

    Rework - changing variables.

--- Day2_part1.py
+++ Day2_part1.py
@@ -3,18 +3,22 @@
 def read_puzzle_input(filename):
     with open(filename, 'r') as file:
         data = file.read().strip()
-    
+
     return(data)
 
 
 def main():
     data = read_puzzle_input('input2day.txt')
 
-    checksum = 0
+    result = 0
+
     for line in data.split('\n'):
-        ll = [int(x) for x in line.split('\t')]
-        checksum += max(ll) - min(ll)
-    print (checksum)
+
+        list_of_numbers = [int(number) for number in line.split('\t')]
+        result += max(list_of_numbers) - min(list_of_numbers)
+
+        print(result)
+
 
 if __name__ == '__main__':
     main()
